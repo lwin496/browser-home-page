47e3b5a428284196954d5f1f855bbc0e62547bca
Louis Win <88466438+lwin496@users.noreply.github.com>
2023-01-26 13:35:07 -0700

    got rid of motion in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Clock from './Components/Clock';
 import './App.css';
 import IconBox from './Components/IconBox';
 import Spotify from 'react-spotify-embed'
-import {motion} from 'framer-motion'
+// import {motion} from 'framer-motion'
 
 
 function App() {
